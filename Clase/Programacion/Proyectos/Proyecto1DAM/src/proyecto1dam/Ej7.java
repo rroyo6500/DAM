31f2eee9178623c2402bd47964b1e6c8cb2e6857
r.royo <rroyo@proton.me>
2025-09-22 12:06:52 +0200

    .

--- Clase/Programacion/Proyectos/Proyecto1DAM/src/proyecto1dam/Ej7.java
+++ Clase/Programacion/Proyectos/Proyecto1DAM/src/proyecto1dam/Ej7.java
@@ -11,10 +11,13 @@ public class Ej7 {
      */
     public static void main(String[] args) {
         
-        int seg = 172800;
+        int seg = 3800;
         
         int hores = seg / 3600;
+        seg -= hores * 3600;
+        
         int min = seg / 60;
+        seg -= min * 60;
         
         System.out.println("Hores: " + hores + " | Minuts: " + min + " | segons: " + seg);
         
